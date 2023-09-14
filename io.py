152aecc434bc24a28150bc3b06d191e5f57d4514
unknown <konradczater@gmail.com>
2023-09-14 10:26:16 +0200

    dodanie typow

--- io.py
+++ io.py
@@ -9,3 +9,13 @@ trzecia linijka
 #funkcja wyjścia
 print("jakis tekst") #funkcja wyjścia
 print("edycja na rpst github")
+
+a = 3
+b = 3.
+c = "tekst"
+d = [3, 5, 'hhh']
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
