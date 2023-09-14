164ec54de20373426fd0b7115ca4005c912127ef
unknown <konradczater@gmail.com>
2023-09-14 10:31:19 +0200

    Prosty kalkulator

---
+++ io2.py
@@ -0,0 +1,14 @@
+'''
+Title: Simple calculator
+Python ver. 3.11
+KP 2023
+'''
+
+x = float(input("Podaj x = "))
+y = float(input("Podaj y = "))
+
+#obliczenia
+suma = x+y
+roznica =
+iloczyn = 
+iloraz = 
