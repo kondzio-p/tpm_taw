0fee9712044f1c9cf1fe4975619becdde8218af8
unknown <konradczater@gmail.com>
2023-09-21 09:21:21 +0200

    listy2

--- listy2.py
+++ listy2.py
@@ -1,32 +1,67 @@
 '''
-Title: Typ sekwencyjny - listy
+Listy jako obiekty
 '''
 
 '''
-lista1 = [1,2,3,4,5]
-lista2 = [1,2,3, "tekst"]
-lista3 = [1,2,3.4,[1,2,"coś"]]
+lista1=[2,3,5,7,9]
 
 print(type(lista1))
-print(type(lista2))
-print(type(lista3))
+print(lista1)
 
-print(type(lista2[1]))
-print(lista3[3][-1])
+lista1.append(33)
+print(lista1)
 
-#lewa 0 1 2 3 4 5...
-#prawa ... -4 -3 -2 -1
+lista1.pop()
+print(lista1)
 
-print(lista2[-1])
+lista1.remove(5)
+print(lista1)
+
+lista2=[]
+lista2=lista1
+
+print(lista2)
+
+lista1.append(33)
+print(lista2)
+
+lista3= lista1.copy()
+print(lista3)
+
+lista1.append(44)
+print(lista1)
+print(lista3)
+
+lista4=[]
+lista4.extend[lista1]
+print(lista1)
 '''
 
-moja_lista=[1,4,2,5,3,6,3]
-print(moja_lista[1:4])
+lista1=[2,3,5,7,9]
+lista5 = []
+#dodaj 6 do listy 5
+#poszerz liste 5 o elementy z listy 1 w kolejności 7,5,3
 
-print(moja_lista[-2:-5:-1])
-print(moja_lista[::-2])
+lista5.append(6)
+lista5.extend(lista1[-2:-5:-1])
+print(lista5)
 
-nowa = ['a','b','c','d','e','f']
-print(nowa[1::2]) #B D F  [START:KONIEC:KROK]
-print(nowa[-2::-2])
-print(nowa[-1::-2])
+lista5.clear()
+print(lista5)
+
+lista1.insert(2,0)
+print(lista1)
+
+#count
+m = lista1.count(3)
+print(f"Trójek w liście jest {m}")
+n=len(lista1)
+print(f"elementów jest w liście {n}")
+
+#reverse
+lista1.reverse()
+print(lista1)
+
+#sort
+lista1.sort()
+print(lista1)
